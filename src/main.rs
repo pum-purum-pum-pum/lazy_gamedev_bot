6af16b737fa6405038403da1f903803a80782416
Vlad <vladzhukovtirko@gmail.com>
2020-02-20 01:06:25 +0300

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,87 @@
 #[macro_use]
 extern crate log;
 
-use std::env;
 use std::collections::HashMap;
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::str::Split;
+use std::time::Duration;
 
+use chrono::{DateTime, Datelike, NaiveDate, NaiveTime, NaiveDateTime, Timelike, Utc, Weekday};
 use env_logger::Builder;
-use log::LevelFilter;
-use ron::ser::{to_string_pretty, PrettyConfig};
-use ron::de::from_str;
-use serde::{Serialize, Deserialize};
 use futures::StreamExt;
-use telegram_bot::*;
-use telegram_bot::types::refs::{GroupId, SupergroupId, UserId};
+use log::LevelFilter;
+use once_cell::sync::Lazy;
+use ron::de::from_str;
+use ron::ser::{to_string_pretty, PrettyConfig};
+use serde::{Deserialize, Serialize};
 use telegram_bot::types::chat::MessageChat;
-use chrono::{DateTime, Datelike, Timelike, Utc, NaiveDate, NaiveTime, Weekday};
+use telegram_bot::types::refs::ChatId;
+use telegram_bot::types::refs::{GroupId, SupergroupId, UserId, ChatRef};
+use telegram_bot::types::requests::send_message::SendMessage;
+use telegram_bot::*;
+use tokio;
+use tokio::sync::Mutex;
+use tokio::time::delay_for;
 
-const MOSCOW_OFFSET: usize = 3;
+const MOSCOW_OFFSET: i64 = 3;
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-#[derive(Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-enum ChatId {
-	SG(SupergroupId),
-	U(UserId),
-	G(GroupId)
-}
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Timer {
-	pub name: String,
-	pub msg: String,
-	pub week_day: Weekday,
-	pub time: NaiveTime,
+    pub name: String,
+    pub msg: String,
+    pub week_day: Weekday,
+    pub time: NaiveTime,
+    pub last_time: Option<DateTime<Utc>>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 struct Chats {
-	pub reminders: HashMap<ChatId, Vec<Timer>>
+    pub reminders: HashMap<ChatId, Vec<Timer>>,
 }
 
-fn parse_request(mut tokens: Split<'_, &str>) -> Result<(Weekday, NaiveTime)> {
+static DATA_PATH: &'static str = "chats.ron";
+
+static CHATS: Lazy<Mutex<Chats>> = Lazy::new(|| {
+    let pretty = PrettyConfig {
+        depth_limit: 2,
+        separate_tuple_members: true,
+        enumerate_arrays: true,
+        ..PrettyConfig::default()
+    };
+    let chats = if let Ok(mut file) = File::open(DATA_PATH) {
+        let mut data = String::new();
+        file.read_to_string(&mut data)
+            .expect("failed to read saved data");
+        from_str(&data).expect("failed to parse saved data")
+    } else {
+        let mut file = File::create(DATA_PATH).expect("failed to create file");
+        let chats = Chats::default();
+        file.write_all(
+            to_string_pretty(&chats, pretty)
+                .expect("failed to format")
+                .as_bytes(),
+        )
+        .expect("failed to create byts array");
+        chats
+    };
+    Mutex::new(chats)
+});
+
+fn parse_request(mut tokens: Split<'_, &str>) -> Result<(Weekday, NaiveTime, String)> {
     // let mut tokens = input.split(" ");
     let token = tokens.next().ok_or("no dayweek token")?;
     info!("{}", &token);
     let day_week: Weekday = from_str(&format!("\"{}\"", token))?;
     info!("{:?}", day_week);
     let token = tokens.next().ok_or("no time token")?;
-	info!("{}", &token);
+    info!("{}", &token);
     let time = NaiveTime::parse_from_str(token, "%H:%M:%S")?;
-    Ok((day_week, time))
+    let token = tokens.next().ok_or("no msg token")?;
+
+    Ok((day_week, time, token.to_string()))
 }
 
 fn update_file(data_path: &str, chats: &Chats) -> Result<()> {
@@ -61,47 +91,52 @@ fn update_file(data_path: &str, chats: &Chats) -> Result<()> {
         enumerate_arrays: true,
         ..PrettyConfig::default()
     };
-	let mut file = File::open(data_path)?;
-	file.write_all(to_string_pretty(chats, pretty)?.as_bytes())?;
-	Ok(())
+    let mut file = File::create(data_path)?;
+    file.write_all(to_string_pretty(chats, pretty)?.as_bytes())?;
+    Ok(())
+}
+
+async fn reminder(api: Api) {
+	let mut last_time_log: DateTime<Utc> = Utc::now() + chrono::Duration::hours(MOSCOW_OFFSET);
+    loop {
+    	{
+	    	for (id, timers) in CHATS.lock().await.reminders.iter_mut() {
+		    	let now: DateTime<Utc> = Utc::now() + chrono::Duration::hours(MOSCOW_OFFSET);
+		    	for timer in timers.iter_mut() {
+		    		let naive_dt = NaiveDateTime::new(now.date().naive_utc(), timer.time);
+		    		let dt = DateTime::<Utc>::from_utc(naive_dt, Utc);
+		    		let d_sec = (now - dt).num_seconds();
+		    		let need_remind = timer.last_time.map(|t| (now - t).num_seconds() > 100).unwrap_or(true);
+		    		if d_sec.abs() < 1 && need_remind {
+		    			timer.last_time = Some(now);
+				    	let chat_ref = ChatRef::from_chat_id(*id);
+				    	let msg = SendMessage::new(chat_ref, format!("{}: {}", &timer.name, &timer.msg));
+				    	let _err = api.send(msg).await;
+		    		}
+		    	}
+	    	}
+	    	// unlock mutex
+    	}
+    	delay_for(Duration::from_millis(100)).await;
+    	let now: DateTime<Utc> = Utc::now() + chrono::Duration::hours(MOSCOW_OFFSET);
+    	if (now - last_time_log).num_seconds() > 10 {
+	    	info!("{:?}", now);
+    		last_time_log = now;
+    	}
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-	let mut builder = Builder::new();
+    let mut builder = Builder::new();
     builder.filter_level(LevelFilter::Info).init();
-    let pretty = PrettyConfig {
-        depth_limit: 2,
-        separate_tuple_members: true,
-        enumerate_arrays: true,
-        ..PrettyConfig::default()
-    };
-    // dbg!(to_string_pretty(&Weekday::Mon, pretty));
-	// return Ok(());
-	// let now = Utc::now();
-	// let (is_pm, hour) = now.hour12();
-    // println!(
-    //     "The current UTC time is {:02}:{:02}:{:02} {}",
-    //     hour,
-    //     now.minute(),
-    //     now.second(),
-    //     if is_pm { "PM" } else { "AM" }
-    // );
 
-    let data_path = "chats.ron";
-    let mut chats = if let Ok(mut file) = File::open(data_path) {
-    	let mut data = String::new();
-    	file.read_to_string(&mut data)?;
-    	from_str(&data)?
-    } else {
-    	let mut file = File::create(data_path)?;
-    	let chats = Chats::default();
-    	file.write_all(to_string_pretty(&chats, pretty)?.as_bytes())?;
-    	chats
-    };
     let token = env::var("LAZY_TOKEN").expect("token not set");
     let api = Api::new(token);
-
+    let a = api.clone();
+    tokio::spawn(async move {
+    	reminder(a).await
+    });
     // Fetch new updates via long poll method
     let mut stream = api.stream();
     // api.send(message.text_reply("hello".to_string()));
@@ -110,42 +145,59 @@ async fn main() -> Result<()> {
         let update = update?;
         if let UpdateKind::Message(message) = update.kind {
             if let MessageKind::Text { ref data, .. } = message.kind {
-                // Print received text message to stdout.
-                let chat_id = match &message.chat {
-                	MessageChat::Private(user) => {
-                		ChatId::U(user.id)
-                	}
-                	MessageChat::Supergroup(group) => {
-                		ChatId::SG(group.id)
-                	}
-                	MessageChat::Group(group) => {
-                		ChatId::G(group.id)
-                	}
-                	_ => continue
-                };
-                // println!("<{}>: {}", &message.from.first_name, data);
+                let chat_id = message.chat.id();
                 let mut tokens = data.split(" ");
-                if let Some(cmd) = tokens.next() { // first token
-                	dbg!(&cmd);
-                	if cmd == "/remind" { // set reminder
-		                match parse_request(tokens) {
-		                	Ok((week_day, time)) => {
-			                	let chat_reminds = chats.reminders.entry(chat_id).or_insert(vec![]);
-			                	chat_reminds.push(Timer {
-			                		name: "".to_string(),
-			                		msg: "".to_string(),
-			                		week_day,
-			                		time
-			                	});
-			                	// dbg!(&chats);
-			                	let _err = update_file(data_path, &chats);
-		                	}
-		                	Err(err) => {
-		                		dbg!(err);
-		                	}
-		                }
-                	}
-                } else {continue;}
+                if let Some(cmd) = tokens.next() {
+                    // first token
+                    info!("{:?}", &cmd);
+                    let cmd = cmd.to_string();
+                    match cmd.as_ref() {
+                        "/remind" => {
+                            // set reminder
+                            match parse_request(tokens) {
+                                Ok((week_day, time, msg)) => {
+                                	info!("Updating");
+                                    let mut chats = CHATS.lock().await;
+
+                                    let chat_reminds =
+                                        chats.reminders.entry(chat_id).or_insert(vec![]);
+                                    chat_reminds.push(Timer {
+                                        name: "".to_string(),
+                                        msg,
+                                        week_day,
+                                        time,
+                                        last_time: None,
+                                    });
+                                    let _err = update_file(DATA_PATH, &*chats);
+                                    info!("{:?}", _err);
+                                    info!(
+                                        "Updated chats {:?}",
+                                        chats.reminders.get(&chat_id)
+                                    );
+                                }
+                                Err(err) => {
+                                    dbg!(err);
+                                }
+                            }
+                        }
+                        "/remind_state" => {
+                            api.send(message.text_reply(format!(
+                                "Current state is: ```{:?}```",
+                                &CHATS.lock().await.reminders.get(&chat_id)
+                            )))
+                            .await?;
+                        }
+                        "/remind_help" => {
+                            api.send(message.text_reply(
+                                "/remind <Weekday: 3 letters Mon, ...> HH:MM:SS".to_string(),
+                            ))
+                            .await?;
+                        }
+                        _ => (),
+                    }
+                } else {
+                    continue;
+                }
                 // Answer message with "Hi".
                 api.send(message.text_reply(format!(
                     "Hi, {}! You just wrote '{}'",
